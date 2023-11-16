48a571c5fb7798d080c6a2f02809199f9df56410
九级浪 <413612464@qq.com>
2023-11-16 11:59:18 +0800

    feat: JSX基础-识别js表达式

--- src/App/01-JSX基础-识别js表达式.js
+++ src/App/01-JSX基础-识别js表达式.js
@@ -1,8 +1,35 @@
 // 01-JSX基础-识别js表达式
+
+const count = 1024;
+
+function getName() {
+  return 'hxl';
+}
+
 function App() {
   return (
     <div className="App">
       app.js
+      <br />
+
+      {/* 传递字符串 */}
+      {'这上字文本'}
+      <br />
+
+      {/* 使用js变量 */}
+      {count}
+      <br />
+
+      {/* 函数调用 */}
+      {getName()}
+      <br />
+
+      {/* 方法调用 */}
+      {new Date().getFullYear()}
+      <br />
+
+      {/* 使用js对象 */}
+      <div style={{ color: 'red' }}>外层花括号是jsx语法，内层花括号是js对象</div>
     </div>
   );
 }
