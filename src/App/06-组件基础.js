4ee6897d665c24d45039fd0f088ed264813bae12
九级浪 <413612464@qq.com>
2023-11-16 13:24:47 +0800

    feat: 组件基础

--- src/App/06-组件基础.js
+++ src/App/06-组件基础.js
@@ -1,9 +1,21 @@
 // 06-组件基础
 
+// 定义组件
+function Button() {
+  const handleClick = () => console.log('点击按钮');
+
+  return <button onClick={handleClick}>点击</button>;
+}
+
 function App() {
   return (
     <div className="App">
+      {/* 自闭合 */}
+      <Button />
+      <br />
 
+      {/* 成双标签 */}
+      <Button></Button>
     </div>
   );
 }
