a28558a51bc091b4f273052637fe136357316fbe
九级浪 <413612464@qq.com>
2023-11-16 17:09:42 +0800

    init: 评论案例-列表渲染

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App/09-样式控制';
+import App from './App/10-评论案例-列表渲染';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
