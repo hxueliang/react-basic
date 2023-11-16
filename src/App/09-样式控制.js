0f2937ac8e483e50f52179197dfae5be548c1315
九级浪 <413612464@qq.com>
2023-11-16 14:19:04 +0800

    init: 样式控制

---
+++ src/App/09-样式控制.js
@@ -0,0 +1,10 @@
+// 09-样式控制
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App/08-useState修改状态的规则';
+import App from './App/09-样式控制';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
