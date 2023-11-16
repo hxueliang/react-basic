ef2754e19e11b7934a50207e7480033a538349f8
九级浪 <413612464@qq.com>
2023-11-16 12:07:07 +0800

    feat: JSX基础-列表渲染

--- src/App/02-JSX基础-列表渲染.js
+++ src/App/02-JSX基础-列表渲染.js
@@ -1,9 +1,20 @@
 // 02-JSX基础-列表渲染
 
+const list = [
+  { id: 1, name: 'html' },
+  { id: 2, name: 'css' },
+  { id: 3, name: 'js' },
+];
+
 function App() {
   return (
     <div className="App">
       app.js
+
+      {/* 列表渲染 */}
+      <ul>
+        {list.map(item => <li key={item.id}>{item.name}</li>)}
+      </ul>
     </div>
   );
 }
