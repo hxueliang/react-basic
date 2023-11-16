d2f715002dc3c5359bc8ea681ac41e325e5fc94c
九级浪 <413612464@qq.com>
2023-11-16 12:21:58 +0800

    feat: JSX基础-条件渲染

--- src/App/03-JSX基础-条件渲染.js
+++ src/App/03-JSX基础-条件渲染.js
@@ -1,7 +1,23 @@
+// 03-JSX基础-条件渲染
+
+const random = Math.random();
+const isLogin = random > 0.5;
+
 function App() {
   return (
     <div className="App">
       app.js
+      <br />
+
+      {random.toFixed(2)}
+      <br />
+
+      {/* 逻辑与 && */}
+      {isLogin && <span>欢迎回来</span>}
+      <br />
+
+      {/* 三元运算 */}
+      {isLogin ? <span>已登录</span> : <span>请登录</span>}
     </div>
   );
 }
