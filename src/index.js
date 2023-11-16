ea4a27ef0c111f6c8e05086ce1a9a683bd5ce826
九级浪 <413612464@qq.com>
2023-11-16 12:12:13 +0800

    init: JSX基础-条件渲染

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App/02-JSX基础-列表渲染';
+import App from './App/03-JSX基础-条件渲染';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
