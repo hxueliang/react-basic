46ee7a255353970570757cd3a3dc8e50a6d4423a
九级浪 <413612464@qq.com>
2023-11-20 13:04:32 +0800

    init: 获取DOM

---
+++ src/App/12-获取DOM.js
@@ -0,0 +1,10 @@
+// 12-获取DOM
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App/11-表单受控绑定';
+import App from './App/12-获取DOM';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
