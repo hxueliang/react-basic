50ea92e6592101b677dc9abd8435364930fa6cf1
九级浪 <413612464@qq.com>
2023-11-20 16:06:50 +0800

    feat: 父子组子通信-子传父

--- src/App/15-父子组子通信-子传父.js
+++ src/App/15-父子组子通信-子传父.js
@@ -1,14 +1,26 @@
 // 15-父子组子通信-子传父
 
+import { useState } from "react";
+
 function Son(props) {
+  console.log(props);
+  const msg = 'this is son msg';
   return <button
+    onClick={() => props.onGetSonMsg(msg)}
   > this is son</button >;
 }
 
 function App() {
+  const [msg, setMsg] = useState('');
+  const getMsg = (data) => {
+    console.log(data);
+    setMsg(data);
+  };
   return (
     <div className="App">
+      <p>{msg}</p>
       <Son
+        onGetSonMsg={getMsg}
       >
         this is span
       </Son>
