7f44c15d3671f55699357e1b6b3d77bba6fcd836
九级浪 <413612464@qq.com>
2023-11-20 13:17:08 +0800

    feat: 获取DOM

--- src/App/12-获取DOM.js
+++ src/App/12-获取DOM.js
@@ -1,8 +1,16 @@
 // 12-获取DOM
+import { useRef } from "react";
 
 function App() {
+  const inputRef = useRef(null);
+  const getDom = () => {
+    console.log(inputRef.current);
+    console.log(inputRef.current.value);
+  };
   return (
     <div className="App">
+      <input ref={inputRef} />
+      <button onClick={getDom}>获取DOM</button>
     </div>
   );
 }
