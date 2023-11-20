6ac2928d71326307054a4a9c12add53d9bfa5fa6
九级浪 <413612464@qq.com>
2023-11-20 11:48:44 +0800

    init: 表单受控绑定

---
+++ src/App/11-表单受控绑定.js
@@ -0,0 +1,10 @@
+// 11-表单受控绑定
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App/10-评论案例-列表渲染';
+import App from './App/11-表单受控绑定';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
