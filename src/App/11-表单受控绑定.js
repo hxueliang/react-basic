3e19027ce246e49dd972542d1644610f77e6a358
九级浪 <413612464@qq.com>
2023-11-20 12:32:52 +0800

    feat: 表单受控绑定

--- src/App/11-表单受控绑定.js
+++ src/App/11-表单受控绑定.js
@@ -1,8 +1,16 @@
 // 11-表单受控绑定
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState('');
   return (
     <div className="App">
+      {value}
+      <br />
+      <input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
     </div>
   );
 }
