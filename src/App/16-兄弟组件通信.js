18d786d0eed14338610d6f41cc4c835a1840ba8a
九级浪 <413612464@qq.com>
2023-11-20 16:33:13 +0800

    feat: 兄弟组件通信-通过状态提升实现

--- src/App/16-兄弟组件通信.js
+++ src/App/16-兄弟组件通信.js
@@ -1,29 +1,38 @@
-// 16-兄弟组件通信
+// 16-兄弟组件通信-通过状态提升实现
 
-function A() {
+import { useState } from "react";
+
+function A({ onGetAName }) {
+  const name = 'this is A name';
   return (
     <div>
       this is A comment,
       <button
+        onClick={() => onGetAName(name)}
       >send</button>
     </div>
   );
 }
 
-function B() {
+function B({ name }) {
   return (
     <div>
       this is B comment,
+      {name}
     </div>
   );
 }
 
 function App() {
+  const [name, setName] = useState('');
+  const getAName = data => {
+    setName(data);
+  };
   return (
     <div className="App">
       this is App
-      <A />
-      <B />
+      <A onGetAName={getAName} />
+      <B name={name} />
     </div>
   );
 }
