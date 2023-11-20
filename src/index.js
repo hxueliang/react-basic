dc057c1aa6cc6c38241925f2180e77adb55f836e
九级浪 <413612464@qq.com>
2023-11-20 17:19:43 +0800

    init: useEffect-基础

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App/17-跨层组件通信';
+import App from './App/18-useEffect-基础';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
