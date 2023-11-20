c977d94336e3a493240f0d7326b5caf3ddf22802
九级浪 <413612464@qq.com>
2023-11-20 15:44:46 +0800

    feat: 父子组子通信-父传子chilren说明

--- src/App/14-父子组子通信-父传子chilren说明.js
+++ src/App/14-父子组子通信-父传子chilren说明.js
@@ -1,13 +1,14 @@
 // 14-父子组子通信-父传子chilren说明
 
 function Son(props) {
-  return <div>this is son</div>;
+  return <div>this is son, {props.children}</div>;
 }
 
 function App() {
   return (
     <div className="App">
       <Son>
+        <span>this is span</span>
       </Son>
     </div>
   );
