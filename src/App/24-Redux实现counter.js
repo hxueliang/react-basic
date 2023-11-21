808c5cb4bc84845380026ad5984134320e07803b
九级浪 <413612464@qq.com>
2023-11-21 16:46:21 +0800

    init: Redux实现counter

---
+++ src/App/24-Redux实现counter.js
@@ -0,0 +1,10 @@
+// 24-Redux实现counter
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App/23-Redux环境准备';
+import App from './App/24-Redux实现counter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
