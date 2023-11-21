6d6ad5117621830eb648f9c97808221f53aafb7f
九级浪 <413612464@qq.com>
2023-11-21 11:21:07 +0800

    init: useEffect-依赖项说明

---
+++ src/App/19-useEffect-依赖项说明.js
@@ -0,0 +1,10 @@
+// 19-useEffect-依赖项说明
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App/18-useEffect-基础';
+import App from './App/19-useEffect-依赖项说明';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
