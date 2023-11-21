31eb94e212ee4e6d805cd54c1046bb84af386599
九级浪 <413612464@qq.com>
2023-11-21 12:47:48 +0800

    feat: Hook的使用规则

--- src/App/22-Hook的使用规则.js
+++ src/App/22-Hook的使用规则.js
@@ -5,8 +5,19 @@
  * 2、只能在组件的顶层调用，不能嵌套在if、for、其他函数中
  */
 
+import { useState } from "react";
+
+// 错误用法1：
+// React Hooks must be called in a React function component or a custom React Hook function.
+// const [value, setValue] = useState('');
 
 function App() {
+  // 错误用法2：
+  // React Hooks must be called in the exact same order in every component render.
+  // if (Math.random() > 0.5) {
+  //   const [value, setValue] = useState('');
+  // }
+
   return (
     <div className="App">
       this is div
