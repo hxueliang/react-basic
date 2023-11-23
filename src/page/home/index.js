ed5c4b28ea9483ca8b10597d6f9a50c01501e34f
九级浪 <413612464@qq.com>
2023-11-23 15:20:10 +0800

    feat: Router-路由导航

--- src/page/home/index.js
+++ src/page/home/index.js
@@ -1,7 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
 function Home() {
+  const navigator = useNavigate();
+
   return (
     <div className="home">
       home
+      <br />
+      {/* 编程式导航 */}
+      <button onClick={() => navigator('/login')}>login</button>
     </div>
   );
 }
--- src/page/login/index.js
+++ src/page/login/index.js
@@ -1,7 +1,17 @@
+import { Link, useNavigate } from "react-router-dom";
+
 function Login() {
+  const navigator = useNavigate();
+
   return (
     <div className="login">
       login
+      <br />
+      {/* 声明式导航 */}
+      <Link to="/home">home</Link>
+      <br />
+      {/* 编程式导航 */}
+      <button onClick={() => navigator('/home')}>home</button>
     </div>
   );
 }
