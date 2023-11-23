0922964681d3bbf6af7a9ebd9535e046e3328cc0
九级浪 <413612464@qq.com>
2023-11-23 17:19:20 +0800

    init: 计事本案例

---
+++ src/App/40-计事本案例.js
@@ -0,0 +1,14 @@
+/**
+ * 40-计事本案例
+ * 
+ */
+
+function App() {
+  return (
+    <div className="App">
+      计事本案例
+    </div>
+  );
+}
+
+export default App;
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 
-import App from '../App/34-Router-嵌套路由';
+import App from '../App/40-计事本案例';
 import Login from '../page/login';
 import Home from '../page/home';
 
