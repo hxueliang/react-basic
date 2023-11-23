7b2d2844ad2bfbf8259500aca4542512641edfa8
九级浪 <413612464@qq.com>
2023-11-23 14:54:26 +0800

    init: Router-快速体验

---
+++ src/App/31-Router-快速体验.js
@@ -0,0 +1,10 @@
+// 31-Router-快速体验
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
-import App from './App/30-美团外卖案例';
+import App from './App/31-Router-快速体验';
 import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
