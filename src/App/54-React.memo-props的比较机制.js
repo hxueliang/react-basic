2e88ea3b3920a988c4abebd09f430e990fdf299c
九级浪 <413612464@qq.com>
2023-12-07 15:55:04 +0800

    feat: React.memo-props的比较机制

--- src/App/54-React.memo-props的比较机制.js
+++ src/App/54-React.memo-props的比较机制.js
@@ -2,29 +2,44 @@
  * 54-React.memo-props的比较机制
  */
 
-import { memo, useState } from "react";
+import { memo, useMemo, useState } from "react";
 
 const MemoSon = memo(
-  function Son() {
+  function Son(porps) {
     console.log('Son组件渲染');
 
     return <div>
       this is son
+      - {porps.count}
     </div>;
   }
 );
+
+const list2 = [1, 2, 3];
 
 function App() {
   console.log('App组件重新渲染');
 
   const [count, setCount] = useState(0);
 
+  const [countSon, setCountSon] = useState(0);
+
+  const num = 100;
+  const list1 = [1, 2, 3];
+  const list3 = useMemo(() => [1, 2, 3], []);
+
   return (
     <div className="App">
       this is app
       <br />
       <button onClick={() => setCount(count + 1)}>+ {count}</button>
-      <MemoSon />
+      <br />
+      <button onClick={() => setCountSon(countSon + 1)}>+ son {countSon}</button>
+      {/* <MemoSon count={countSon} /> */}
+      {/* <MemoSon count={num} /> */}
+      {/* <MemoSon count={list1} /> */}
+      {/* <MemoSon count={list2} /> */}
+      <MemoSon count={list3} />
     </div>
   );
 }
