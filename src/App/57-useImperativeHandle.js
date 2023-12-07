27e4dfd940b172f357f510a98f3c9a743fb01c72
九级浪 <413612464@qq.com>
2023-12-07 17:18:23 +0800

    feat: useInperativeHandle

--- src/App/57-useImperativeHandle.js
+++ src/App/57-useImperativeHandle.js
@@ -1,14 +1,25 @@
 // 57-useImperativeHandle
 
-import { forwardRef, useRef } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
 
 const Son = forwardRef((props, ref) => {
-  return <input ref={ref} />;
+  const inputRef = useRef(null);
+  const focusHandler = () => {
+    inputRef.current.focus();
+  };
+  // 暴露子组件方法
+  useImperativeHandle(ref, () => {
+    return {
+      focusHandler,
+    };
+  });
+  return <input ref={inputRef} />;
 });
 
 function App() {
   const sonRef = useRef(null);
   const onClick = () => {
+    sonRef.current.focusHandler();
   };
   return (
     <div className="App">
