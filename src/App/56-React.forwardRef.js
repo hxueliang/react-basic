8f2179039b671e66722b33269dba261e26561dfe
九级浪 <413612464@qq.com>
2023-12-07 16:54:26 +0800

    feat: React.forwardRef

--- src/App/56-React.forwardRef.js
+++ src/App/56-React.forwardRef.js
@@ -1,14 +1,25 @@
 // 56-React.forwardRef
 
-function Son() {
-  return <input />;
-}
+import { forwardRef, useRef } from "react";
+
+// function Son() {
+//   return <input />;
+// }
+
+const Son = forwardRef((props, ref) => {
+  return <input ref={ref} />;
+});
 
 function App() {
+  const sonRef = useRef(null);
+  const onClick = () => {
+    console.log(sonRef);
+    sonRef.current.focus();
+  };
   return (
     <div className="App">
-      <Son />
-      <button>focus</button>
+      <Son ref={sonRef} />
+      <button onClick={onClick}>focus</button>
     </div>
   );
 }
