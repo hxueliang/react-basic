2c28ea83b1472f8dfcf7bb1f3416ced6849c0a98
九级浪 <413612464@qq.com>
2023-12-08 13:08:25 +0800

    feat: zustand快速上手

--- src/App/61-zustand快速上手.js
+++ src/App/61-zustand快速上手.js
@@ -1,8 +1,26 @@
 // 61-zustand快速上手
 
+import { create } from "zustand";
+
+const useStore = create(set => ({
+  count: 0,
+  // 需要原数据
+  inc: () => set(state => ({ count: state.count + 1 })),
+  // 固定改为10
+  inc10: () => set({ count: 10 }),
+  // 根据参数修改
+  incSet: (num) => set({ count: num })
+}));
+
 function App() {
+  const { count, inc, inc10, incSet } = useStore();
+
   return (
     <div className="App">
+      {count}
+      <button onClick={inc}>+1，需要原数据</button>
+      <button onClick={inc10}>固定10</button>
+      <button onClick={() => incSet(100)}>传参100</button>
     </div>
   );
 }
