01a96e99b9beb678b4a3f5fda4e484c63c22cc30
九级浪 <413612464@qq.com>
2023-12-08 12:10:02 +0800

    feat: classAPI

--- src/App/58-classAPI.js
+++ src/App/58-classAPI.js
@@ -1,8 +1,29 @@
 // 58-classAPI
 
+import { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+  };
+
+  setCount(num) {
+    this.setState({
+      count: this.state.count + num
+    });
+  };
+
+  render() {
+    return <>
+      <button onClick={() => this.setCount(2)}>+2: {this.state.count}</button>
+    </>;
+  }
+}
+
 function App() {
   return (
     <div className="App">
+      <Counter />
     </div>
   );
 }
