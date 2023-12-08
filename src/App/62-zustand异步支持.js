3e10f4f9b66615912e5ed4137bc3474479b31e4f
九级浪 <413612464@qq.com>
2023-12-08 13:39:58 +0800

    feat: zustand异步支持

--- src/App/62-zustand异步支持.js
+++ src/App/62-zustand异步支持.js
@@ -1,8 +1,32 @@
 // 62-zustand异步支持
 
+import { useEffect } from "react";
+import { create } from "zustand";
+
+import { request } from "@/utils";
+
+const useStore = create(set => ({
+  list: [],
+  fetchList: async () => {
+    const res = await request('/channels');
+    set({
+      list: res.data.channels
+    });
+  }
+}));
+
 function App() {
+  const { list, fetchList } = useStore();
+
+  useEffect(() => {
+    fetchList();
+  }, [fetchList]);
+
   return (
     <div className="App">
+      <ul>
+        {list.map(({ id, name }) => <li key={id}>{name}</li>)}
+      </ul>
     </div>
   );
 }
